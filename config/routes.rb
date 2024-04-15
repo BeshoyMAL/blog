85cd3152100fed943c063f64fa8edafdfa7e3836
KenyonBL <157287613+KenyonBL@users.noreply.github.com>
2024-04-14 23:45:19 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
-
-  Rails.application.routes.draw do
+Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-end
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Other routes can be defined here
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
